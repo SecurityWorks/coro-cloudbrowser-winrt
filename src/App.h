add638cd0d97d00bf4724aef1f60f9c95006ab39
Paweł Wegner <pawel.wegner95@gmail.com>
2023-02-03 21:31:02 +0100

    Initial commit.

---
+++ src/App.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+#include <future>
+
+#include "App.xaml.g.h"
+#include "coro/promise.h"
+#include "coro/util/event_loop.h"
+
+namespace winrt::coro_cloudbrowser_winrt::implementation {
+
+struct App : AppT<App> {
+  App();
+  void OnLaunched(
+      Windows::ApplicationModel::Activation::LaunchActivatedEventArgs const &);
+  void OnSuspending(IInspectable const &,
+                    Windows::ApplicationModel::SuspendingEventArgs const &);
+  void OnNavigationFailed(
+      IInspectable const &,
+      Windows::UI::Xaml::Navigation::NavigationFailedEventArgs const &);
+
+ private:
+  coro::Task<> RunHttpServer();
+
+  std::future<void> thread_;
+  coro::Promise<void> semaphore_;
+  coro::util::EventLoop event_loop_;
+};
+
+}  // namespace winrt::coro_cloudbrowser_winrt::implementation
